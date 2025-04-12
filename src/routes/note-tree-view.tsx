807aa0920a4eff53cbf0953f61c311134ed1a03a
Ryan G <exogenesis@protonmail.com>
2025-04-12 21:18:35 +1000

    fix: remove unnecessary ref prop from treeview component

--- src/routes/note-tree-view.tsx
+++ src/routes/note-tree-view.tsx
@@ -88,7 +88,6 @@ export default function NoteTreeView() {
             }}>
               <TreeView 
                 data={treeData()?.tree?.children || []}
-                ref={undefined}
               />
             </div>
           </Show>
